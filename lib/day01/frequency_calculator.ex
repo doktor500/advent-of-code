bdb93c5d98c2ea5817b53f6426adc06d7cbe6dd2
David Molinero <davidmolinero.com@gmail.com>
2018-12-09 14:38:08 +0000

    Refactors

--- lib/day01/frequency_calculator.ex
+++ lib/day01/frequency_calculator.ex
@@ -1,10 +1,7 @@
 defmodule FrequencyCalculator do
   def calculate_final(frequencies) do
     frequencies
-    |> Enum.map(&to_number(&1))
+    |> Enum.map(&String.to_integer/1)
     |> Enum.sum
   end
-
-  defp to_number("+" <> frequency), do: String.to_integer(frequency)
-  defp to_number("-" <> frequency), do: String.to_integer(frequency) * - 1
 end

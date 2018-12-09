18513e74a3e3a85b1b7eb78f8ffee024593a12fa
David Molinero <davidmolinero.com@gmail.com>
2018-12-09 16:42:50 +0000

    Refactors

--- lib/day01/frequency_calculator.ex
+++ lib/day01/frequency_calculator.ex
@@ -16,12 +16,11 @@ defmodule FrequencyCalculator do
 
   defp calculate_first_repeated_frequency(frequency_metadata = %{ remaining: [head | tail] }) do
     current_frequency = String.to_integer(head) + hd(frequency_metadata.seen)
-    seen_frequencies = [current_frequency | frequency_metadata.seen]
 
     frequency_metadata
-    |> Map.put(:seen, seen_frequencies)
+    |> Map.put(:seen, [current_frequency | frequency_metadata.seen])
     |> Map.put(:remaining, tail)
-    |> Map.put(:found, has_duplicates?(seen_frequencies))
+    |> Map.put(:found, Enum.member?(frequency_metadata.seen, current_frequency))
     |> calculate_first_repeated_frequency
   end
 
@@ -29,10 +28,6 @@ defmodule FrequencyCalculator do
     frequency_metadata
     |> Map.put(:remaining, frequency_metadata.all)
     |> calculate_first_repeated_frequency
-  end
-
-  defp has_duplicates?(values) do
-    length(values) != length(Enum.uniq(values))
   end
 
 end
